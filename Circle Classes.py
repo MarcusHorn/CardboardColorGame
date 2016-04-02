04236223075619b324abe1b9c4abb65845c20687
MarcusHorn <marcushorn44@gmail.com>
2016-04-01 21:44:09 -0400

    Update Circle Classes.py

--- Circle Classes.py
+++ Circle Classes.py
@@ -1,2 +1,19 @@
+import math
 class Circle(object):
+    def __init__(self, boardRadius, circleRadius, angle):
+        radianAngle = angle * (math.pi/180) #Used for math.cos and math.sin
+        self.x = (boardRadius + boardRadius*math.cos(radianAngle)) // 1
+        self.y = (boardRadius + boardRadius*math.sin(radianAngle)) // 1
+        self.r = circleRadius
+        
+    def updateCircle(self, dAngle):
+        self.angle += dAngle
+        self.angle %= 360
+        radianAngle = angle * (math.pi/180) #Used for math.cos and math.sin
+        self.x = (boardRadius + boardRadius*math.cos(radianAngle)) // 1
+        self.y = (boardRadius + boardRadius*math.sin(radianAngle)) // 1
+        
+    def getCoords(self):
+        return self.x, self.y
+        
     
